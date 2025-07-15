94cf53ee763deaaa93e8d0af9c2cc3f5a20ccc52
erfan mirasadi <erfan@erfanmacpro.local>
2025-07-16 01:37:44 +0300

    Refactor: Move Supabase credentials to environment variables

--- services/supabase.js
+++ services/supabase.js
@@ -1,8 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://spnkwuvimsrrjketymiq.supabase.co";
-const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNwbmt3dXZpbXNycmprZXR5bWlxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDY1MzY1OTIsImV4cCI6MjA2MjExMjU5Mn0.K8Gx8P7YEGJ9PBzOpzCpv-uS3JMEGVNy8OT6mZgS-dA";
+// Read Supabase credentials from environment variables
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
